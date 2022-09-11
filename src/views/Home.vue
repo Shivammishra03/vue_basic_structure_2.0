0fbb76e4c596d96da8203c7d0864420f7575902c
DESKTOP-QRRIOUT\Intel <shivam@depasserinfotech.in>
2022-09-11 22:30:16 +0530

    changes added(11-09-2022)

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <main class="home-page">
+        <h1 class="home-page-h1">Welcome To Vue js Website</h1>
+        <p class="home-page-p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati in 
+        </p>
+    </main>
+</template>
+<style lang="scss" scoped>
+.home-page-h1{
+    text-align: center;
+    color:  #0000FF;
+}
+.home-page-p
+{
+    color: #3A3B3C;
+    font-size: 32px;
+    text-align: center;
+}
+</style>
---
+++ src/views/Team.vue
@@ -0,0 +1,18 @@
+<template>
+    <main class="about-page">
+        <h1 class="about-page-h1">Team</h1>
+        <p class="about-page-p">this is about page Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur dicta, nobis sit itaque magni distinctio beatae nesciunt dolorum reprehenderit adipisci consequatur harum a, nulla ex perferendis praesentium corporis esse amet.</p>
+    </main>
+</template>
+<style lang="scss">
+    .about-page-h1{
+    text-align: center;
+    color:  #0000FF;
+}
+.about-page-p
+{
+    color: #3A3B3C;
+    font-size: 32px;
+    text-align: center;
+}
+</style>
