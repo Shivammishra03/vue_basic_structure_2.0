ed911146f2e11dccf35c06ee83bd632fe3cfadb1
DESKTOP-QRRIOUT\Intel <shivam@depasserinfotech.in>
2022-11-24 12:57:13 +0530

    changes added

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,10 +1,10 @@
 <template>
-    <aside :class ="`${is_expanded ? 'is-expanded' : ''}`">
+    <aside :class ="`${is_expanded && 'is-expanded'}`">
         <div class="logo">
             <img :src="logoURL" alt="Vue" /> 
         </div>
         <div class="menu-toggle-wrap">
-        <button class="menu-toggle" v-on:click="ToggleMenu">
+        <button class="menu-toggle" @click="ToggleMenu">
             <span class="material-icons">keyboard_double_arrow_right</span>
         </button>
         </div>
@@ -42,7 +42,7 @@ import{ ref } from 'vue'
 import logoURL from '../assets/logo.png'
 
 const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
-const toggleMenu = () => {
+const ToggleMenu = () => {
     is_expanded.value = !is_expanded.value
     localStorage.setItem("is_expanded", is_expanded.value)
 }
